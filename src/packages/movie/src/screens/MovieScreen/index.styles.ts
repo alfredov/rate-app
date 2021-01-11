2049f8edcf97f56a4d3e2962094dd9daa8c7bbc1
Alfredo Villagomez <alfredo.villagomez.m@gmail.com>
2021-01-11 19:59:58 -0400

    connect movie screen with store

---
+++ src/packages/movie/src/screens/MovieScreen/index.styles.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react'
+
+export const cardWrapper  = css`
+  display: flex;
+  gap: 1rem;
+  width: 96vw;
+  overflow-x: scroll;
+`
+
+export default {
+  cardWrapper,
+}
--- src/packages/movie/src/screens/MovieScreen/index.tsx
+++ src/packages/movie/src/screens/MovieScreen/index.tsx
@@ -1,28 +1,47 @@
+/** @jsx jsx */
 import React, { memo, useEffect } from 'react'
+import { jsx } from '@emotion/react'
 import { connect } from 'react-redux'
 
+import { Card } from 'app-ui'
+
+import { RootState } from '../../reducer'
+import { getUpcomingMovies as getUpcomingMoviesSelector } from '../../selectors'
 import { getUpcomingMovies } from '../../actions'
+import { TMovie } from '../../schemas'
+import styles from './index.styles'
 
 type Props = {
-  cancel: () => void,
+  loading: boolean,
+  movies: TMovie[],
   fetchUpcomingMovies: typeof getUpcomingMovies.request,
+  cancel: () => void,
 }
 
-export const MovieScreen: React.FC<Props> = ({ fetchUpcomingMovies, cancel }) => {
+export const MovieScreen: React.FC<Props> = ({ fetchUpcomingMovies, loading, movies, cancel }) => {
   useEffect(() => {
     fetchUpcomingMovies({ page: 1 })
+    return cancel
   }, [])
 
   return (
     <section>
       <h1>Movies🍿</h1>
-      <button onClick={() => fetchUpcomingMovies({ page: 1 })}>Upcoming movies</button>
+      {loading && <h3>Loading..</h3>}
+      <div css={styles.cardWrapper}>
+        {movies.map((movie, index) =>
+          <Card key={index} image={movie.posterPath} title={movie.title} onClick={() => null} />,
+        )}
+      </div>
     </section>
   )
 }
 
 export default connect(
-  null,
+  (state: RootState) => ({
+    loading: getUpcomingMoviesSelector(state).loading,
+    movies: getUpcomingMoviesSelector(state).data,
+  }),
   {
     fetchUpcomingMovies: getUpcomingMovies.request,
     cancel: getUpcomingMovies.cancel,
