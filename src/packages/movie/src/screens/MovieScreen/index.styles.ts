a4daf7f152b16b40bd01eb8ccdddd884312eb7f6
Alfredo Villagomez <alfredo.villagomez.m@gmail.com>
2021-01-11 23:15:19 -0400

    top rated movies in screen

--- src/packages/movie/src/screens/MovieScreen/index.styles.ts
+++ src/packages/movie/src/screens/MovieScreen/index.styles.ts
@@ -1,12 +1,9 @@
 import { css } from '@emotion/react'
 
-export const cardWrapper  = css`
-  display: flex;
-  gap: 1rem;
-  width: 96vw;
-  overflow-x: scroll;
+export const topRated  = css`
+  margin-top: 2rem;
 `
 
 export default {
-  cardWrapper,
+  topRated,
 }
--- src/packages/movie/src/screens/MovieScreen/index.tsx
+++ src/packages/movie/src/screens/MovieScreen/index.tsx
@@ -4,27 +4,49 @@ import { jsx } from '@emotion/react'
 import { connect } from 'react-redux'
 import t from 'format-message'
 
-import { Card, CardPaginator } from 'app-ui'
+import { Card, CardPaginator, CardScode } from 'app-ui'
 
 import { RootState } from '../../reducer'
-import { getUpcomingMovies as getUpcomingMoviesSelector } from '../../selectors'
-import { getUpcomingMovies } from '../../actions'
-import { TMovie } from '../../schemas'
+import {
+  getUpcomingMovies as getUpcomingMoviesSelector,
+  getTopRatedMovies as getTopRatedMoviesSelector,
+} from '../../selectors'
+import { getUpcomingMovies, getTopRatedMovies } from '../../actions'
+import { TMovie, TtopRatedMovie } from '../../schemas'
+import styles from './index.styles'
 
 type Props = {
   loading: boolean,
+  topRatedMoviesLoading: boolean,
   movies: TMovie[],
+  topRatedMovies: TtopRatedMovie[],
   fetchUpcomingMovies: typeof getUpcomingMovies.request,
+  fetchTopRatedMovies: typeof getTopRatedMovies.request,
+  cancelFetchTopRatedMovies: () => void,
   cancel: () => void,
 }
 
-export const MovieScreen: React.FC<Props> = ({ fetchUpcomingMovies, loading, movies, cancel }) => {
+export const MovieScreen: React.FC<Props> = ({
+  fetchUpcomingMovies,
+  fetchTopRatedMovies,
+  topRatedMovies,
+  topRatedMoviesLoading,
+  cancelFetchTopRatedMovies,
+  loading,
+  movies,
+  cancel,
+}) => {
   useEffect(() => {
     fetchUpcomingMovies({ page: 1 })
-    return cancel
+    fetchTopRatedMovies({ page: 1 })
+    return () => {
+      cancel()
+      cancelFetchTopRatedMovies()
+    }
   }, [])
 
   const fetchMore = (pageNumber: number) => fetchUpcomingMovies({ page: pageNumber })
+  const fetchMoreTopRated = (pageNumber: number) => fetchTopRatedMovies({ page: pageNumber })
 
   return (
     <section>
@@ -42,6 +64,24 @@ export const MovieScreen: React.FC<Props> = ({ fetchUpcomingMovies, loading, mov
           />,
         )}
       </CardPaginator>
+      <div css={styles.topRated}>
+        <CardPaginator
+          title={t('Top Rated')}
+          fetchMoreTitle={topRatedMoviesLoading ? t('loading') : t('more')}
+          onPaginate={fetchMoreTopRated}
+        >
+          {topRatedMovies.map((movie, index) => (
+            <CardScode
+              key={index}
+              onClick={() => null}
+              releaseYear={movie.releaseDate}
+              score={movie.score}
+              title={movie.title}
+              url={movie.posterPath}
+            />
+          ))}
+        </CardPaginator>
+      </div>
     </section>
   )
 }
@@ -50,9 +90,13 @@ export default connect(
   (state: RootState) => ({
     loading: getUpcomingMoviesSelector(state).loading,
     movies: getUpcomingMoviesSelector(state).data,
+    topRatedMoviesLoading: getTopRatedMoviesSelector(state).loading,
+    topRatedMovies: getTopRatedMoviesSelector(state).data,
   }),
   {
     fetchUpcomingMovies: getUpcomingMovies.request,
+    fetchTopRatedMovies: getTopRatedMovies.request,
+    cancelFetchTopRatedMovies: getTopRatedMovies.cancel,
     cancel: getUpcomingMovies.cancel,
   },
 )(memo(MovieScreen))
