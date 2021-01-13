b4f3d3994094f29937f87e1259a3e597de972381
Alfredo Villagomez <alfredo.villagomez.m@gmail.com>
2021-01-13 19:03:45 -0400

    Yellowcard updated

--- src/packages/app-ui/src/YellowCard/index.styles.ts
+++ src/packages/app-ui/src/YellowCard/index.styles.ts
@@ -1,17 +1,19 @@
 import { css } from '@emotion/react'
 
-export const card = (theme: Theme) => css`
+type Params = { theme: Theme } & { width?: number, height?: number }
+
+export const card = (params: Params) => css`
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #FECB2F;
   border-radius: 6px;
-  color: ${theme.palette.common.white};
+  color: ${params.theme.palette.common.white};
   font-size: 20px;
   font-weight: 500;
   text-transform: uppercase;
-  height: 210px;
-  min-width: 140px;
+  height: ${params.height ? params.height : 210}px;
+  min-width: ${params.width ? params.width : 140}px;
 
   &:hover {
     cursor: pointer;
--- src/packages/app-ui/src/YellowCard/index.tsx
+++ src/packages/app-ui/src/YellowCard/index.tsx
@@ -5,12 +5,24 @@ import { jsx } from '@emotion/react'
 import styles from './index.styles'
 
 export type Props = {
+  width?: number,
+  height?: number,
   onClick: () => void,
   children: React.ReactNode,
 }
 
-const YellowCard: React.FC<Props> = ({ children, onClick }) => (
-  <div css={(theme: any) => styles.card(theme)} role="button" tabIndex={0} onClick={onClick}>
+const YellowCard: React.FC<Props> = ({
+  children,
+  onClick,
+  width,
+  height,
+}) => (
+  <div
+    css={(theme: any) => styles.card({ theme, width, height })}
+    role="button"
+    tabIndex={0}
+    onClick={onClick}
+  >
     {children}
   </div>
 )
