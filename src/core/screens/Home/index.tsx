c76423fb26bf8f13b12966067bdaa3f0e6d07122
Alfredo Villagomez <alfredo.villagomez.m@gmail.com>
2021-01-13 20:41:40 -0400

    enable current tab selected

--- src/core/screens/Home/index.tsx
+++ src/core/screens/Home/index.tsx
@@ -2,27 +2,42 @@
 
 import React from 'react'
 import { jsx } from '@emotion/react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 import { NavItem } from 'app-ui'
 import { PATH_BASE as MOVIE_PATH_BASE } from 'movie'
+import { PATH_BASE as TV_PATH_BASE } from 'tv'
 
 import styles from './index.styles'
 
-export const Home: React.FC = ({ children }) => (
-  <section css={styles.wrapper}>
-    <section css={(theme: any) => styles.content(theme)}>
-      {children}
+export const Home: React.FC = ({ children }) => {
+  const { pathname } = useLocation()
+
+  return (
+    <section css={styles.wrapper}>
+      <section css={(theme: any) => styles.content(theme)}>
+        {children}
+      </section>
+      <footer css={styles.footer}>
+        <Link to={MOVIE_PATH_BASE}>
+          <NavItem
+            icon="movie"
+            selected={pathname === MOVIE_PATH_BASE}
+          >
+            Movies
+          </NavItem>
+        </Link>
+        <Link to={TV_PATH_BASE}>
+          <NavItem
+            icon="tv"
+            selected={pathname === TV_PATH_BASE}
+          >
+            Tv
+          </NavItem>
+        </Link>
+      </footer>
     </section>
-    <footer css={styles.footer}>
-      <Link to={MOVIE_PATH_BASE}>
-        <NavItem icon="movie" selected>Movies</NavItem>
-      </Link>
-      <Link to="/AV">
-        <NavItem icon="tv" selected={false}>Tv</NavItem>
-      </Link>
-    </footer>
-  </section>
-)
+  )
+}
 
 export default Home
